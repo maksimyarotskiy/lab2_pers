1b6f7daf3ed5c2aa406538fe97809b7363556026
Maksim Yarotskiy <iartoskiimaksim@gmail.com>
2024-10-07 20:02:27 +0300

    стили поменял на более нормальные и подровнял все

--- static/js/script.js
+++ static/js/script.js
@@ -1,54 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Кнопка "Показать больше"
+  const toggleMoreButton = document.getElementById("toggleMore");
+  const moreInfo = document.getElementById("moreInfo");
 
-    // Кнопка "Показать больше"
-    const toggleMoreButton = document.getElementById("toggleMore");
-    const moreInfo = document.getElementById("moreInfo");
-  
-    toggleMoreButton.addEventListener("click", function () {
+  toggleMoreButton.addEventListener("click", function () {
       if (moreInfo.style.display === "none") {
-        moreInfo.style.display = "block";
-        toggleMoreButton.textContent = "Скрыть";
+          moreInfo.style.display = "block";
+          toggleMoreButton.textContent = "Скрыть";
       } else {
-        moreInfo.style.display = "none";
-        toggleMoreButton.textContent = "Показать больше";
+          moreInfo.style.display = "none";
+          toggleMoreButton.textContent = "Показать больше";
       }
-    });
-  
-    // Смена темы
-    const themeSwitcher = document.getElementById("themeSwitcher");
-  
-    themeSwitcher.addEventListener("click", function () {
+  });
+
+  // Смена темы
+  const themeSwitcher = document.getElementById("themeSwitcher");
+
+  themeSwitcher.addEventListener("click", function () {
       document.body.classList.toggle("dark-theme");
       localStorage.setItem("theme", document.body.classList.contains("dark-theme") ? "dark" : "light");
-    });
-  
-    // Проверка темы при загрузке страницы
-    if (localStorage.getItem("theme") === "dark") {
-      document.body.classList.add("dark-theme");
-    }
-  
-    // Слайдер
-    const images = ["../static/images/project1.png", "../static/images/project2.png", "../static/images/project3.png"];
+  });
 
-    let currentImageIndex = 0;
-  
-    const sliderImage = document.getElementById("sliderImage");
-    const prevButton = document.getElementById("prevButton");
-    const nextButton = document.getElementById("nextButton");
-  
-    function updateSlider() {
+  // Проверка темы при загрузке страницы
+  if (localStorage.getItem("theme") === "dark") {
+      document.body.classList.add("dark-theme");
+  }
+
+  // Слайдер
+  const images = ["../static/images/project1.png", "../static/images/project2.png", "../static/images/project3.png"];
+  let currentImageIndex = 0;
+
+  const sliderImage = document.getElementById("sliderImage");
+  const prevButton = document.getElementById("prevButton");
+  const nextButton = document.getElementById("nextButton");
+
+  function updateSlider() {
       sliderImage.src = images[currentImageIndex];
-    }
-  
-    prevButton.addEventListener("click", function () {
+  }
+
+  prevButton.addEventListener("click", function () {
       currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
       updateSlider();
-    });
-  
-    nextButton.addEventListener("click", function () {
+  });
+
+  nextButton.addEventListener("click", function () {
       currentImageIndex = (currentImageIndex + 1) % images.length;
       updateSlider();
-    });
-  
   });
-  
+});
